529908ad2a51e4640c6bf5a6471bf2211548b4c3
Barbara Souza <b.barbaratallyta@gmail.com>
2018-02-27 23:30:52 -0300

    Implemented update func

--- Cars/REST.swift
+++ Cars/REST.swift
@@ -15,7 +15,12 @@ enum CarError{
     case noData
     case responseStatusCode(code: Int)
     case invalidJSON
-    
+}
+
+enum RESTOperation{
+    case save
+    case update
+    case delete
 }
 
 class REST{
@@ -23,7 +28,7 @@ class REST{
     private static let basePath = "https://carangas.herokuapp.com/cars"
     private static let configuration : URLSessionConfiguration = {
         let config = URLSessionConfiguration.default
-        config.allowsCellularAccess = false
+        //config.allowsCellularAccess = false
         config.httpAdditionalHeaders = ["Content-Type":"application/json"]
         config.timeoutIntervalForRequest = 30
         config.httpMaximumConnectionsPerHost = 5
@@ -62,13 +67,66 @@ class REST{
         dataTask.resume()
     }
     
+    class func loadBrand(onComplete: @escaping ([Brand]?) -> Void) {
+        guard let url = URL(string: "https://fipeapi.appspot.com/api/1/carros/marcas.json") else {
+            onComplete(nil)
+            return
+        }
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil{
+                onComplete(nil)
+            }else{
+                guard let response = response as? HTTPURLResponse else {
+                    onComplete(nil)
+                    return
+                }
+                if (response.statusCode == 200){
+                    guard let data = data else {return}
+                    do{
+                        let brands = try JSONDecoder().decode([Brand].self, from: data)
+                        onComplete(brands)
+                    }catch{
+                        print("\(error.localizedDescription)")
+                        onComplete(nil)
+                    }
+                }else{
+                    print("Status inválido!")
+                    onComplete(nil)                }
+            }
+        }
+        dataTask.resume()
+    }
+    
     class func save(car: Car, onComplete: @escaping (Bool) -> Void) {
-        guard let url = URL(string: basePath) else {
+        self.applyOperation(car: car, operation: .save, onComplete: onComplete)
+    }
+    
+    class func update(car: Car, onComplete: @escaping (Bool) -> Void) {
+        self.applyOperation(car: car, operation: .update, onComplete: onComplete)
+    }
+    
+    class func delete(car: Car, onComplete: @escaping (Bool) -> Void) {
+        self.applyOperation(car: car, operation: .delete, onComplete: onComplete)
+    }
+    
+    private class func applyOperation(car: Car, operation: RESTOperation ,onComplete: @escaping (Bool) -> Void){
+        let urlString : String = basePath + "/" + (car._id ?? "")
+        guard let url = URL(string: urlString) else {
             onComplete(false)
             return
         }
+        var httpMethod : String
         var request = URLRequest(url: url)
-        request.httpMethod = "POST"
+        switch operation {
+        case .save:
+            httpMethod = "POST"
+        case .update:
+            httpMethod = "PUT"
+        case .delete:
+            httpMethod = "DELETE"
+        }
+        
+        request.httpMethod = httpMethod
         guard let json = try? JSONEncoder().encode(car) else {
             onComplete(false)
             return
@@ -88,3 +146,4 @@ class REST{
         dataTask.resume()
     }
 }
+
